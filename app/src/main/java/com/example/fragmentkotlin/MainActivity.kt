a863e774db9a2e54616804549d7e9398d4cb0581
Raghav Aggarwal <raghavaggarwal776@gmail.com>
2021-05-29 15:42:18 +0530

    two fragments

--- app/src/main/java/com/example/fragmentkotlin/MainActivity.kt
+++ app/src/main/java/com/example/fragmentkotlin/MainActivity.kt
@@ -2,10 +2,34 @@ package com.example.fragmentkotlin
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.LayoutInflater
+import com.example.fragmentkotlin.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+    lateinit var binding: ActivityMainBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding= ActivityMainBinding.inflate(LayoutInflater.from(this))
+        setContentView(binding.root)
+
+        val firstfragment = Fragment1()
+        val secondfragment = Fragment2()
+
+        binding.b1.setOnClickListener {
+            supportFragmentManager.beginTransaction().apply {
+                replace(R.id.flFragment,firstfragment)
+                addToBackStack(null)
+                commit()
+            }
+        }
+        binding.b2.setOnClickListener {
+            supportFragmentManager.beginTransaction().apply {
+                replace(R.id.flFragment,secondfragment)
+                addToBackStack(null)
+                commit()
+            }
+
+        }
+
     }
 }
