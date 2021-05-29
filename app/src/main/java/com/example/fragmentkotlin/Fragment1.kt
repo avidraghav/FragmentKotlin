13c97cdc906dd594fb83ce2ebaaafd95d347c002
Raghav Aggarwal <raghavaggarwal776@gmail.com>
2021-05-29 14:15:21 +0530

    single fragment

---
+++ app/src/main/java/com/example/fragmentkotlin/Fragment1.kt
@@ -0,0 +1,12 @@
+package com.example.fragmentkotlin
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+
+class Fragment1 : Fragment(R.layout.fragment_1) {
+
+
+}
---
+++ app/src/main/java/com/example/fragmentkotlin/Fragment2.kt
@@ -0,0 +1,6 @@
+package com.example.fragmentkotlin
+
+import androidx.fragment.app.Fragment
+
+class Fragment2:Fragment(R.layout.fragment_2) {
+}
